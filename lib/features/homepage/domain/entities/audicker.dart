55220df4d440b7501bc4a481a8f83f98044b63b4
SBilaal <mbilaalme@gmail.com>
2020-08-23 14:07:30 +0100

    Added Audicker model class.

---
+++ lib/features/homepage/domain/entities/audicker.dart
@@ -0,0 +1,18 @@
+import 'package:audickers/core/enums/file_status.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+class Audicker extends Equatable {
+  final String name;
+  final String path;
+  final FileStatus status;
+
+  Audicker({
+    @required this.name,
+    @required this.path,
+    @required this.status,
+  });
+
+  @override
+  List<Object> get props => [name, path, status];
+}
