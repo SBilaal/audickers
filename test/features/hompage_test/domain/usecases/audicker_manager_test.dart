37648263e83798d352bcdd155a68a40cd93362e5
SBilaal <mbilaalme@gmail.com>
2020-08-25 11:48:02 +0100

    Wrote test for rename method in AudickerManager.

---
+++ test/features/hompage_test/domain/usecases/audicker_manager_test.dart
@@ -0,0 +1,29 @@
+import 'package:audickers/core/enums/file_status.dart';
+import 'package:audickers/features/homepage/domain/entities/audicker.dart';
+import 'package:audickers/features/homepage/domain/repositories/audicker_repository.dart';
+import 'package:audickers/features/homepage/domain/usecases/audicker_manager.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAudickerRepository extends Mock implements AudickerRepository {}
+
+void main() {
+  AudickerManager manager;
+  MockAudickerRepository repository;
+
+  setUp((){
+    repository = MockAudickerRepository();
+    manager = AudickerManager(repository);
+  });
+
+  test("should rename the file passed into it", () async {
+    var audicker = Audicker(name: "aud", path: "Whatsapp/aud.opus", status: FileStatus.Raw);
+    var newAudicker = Audicker(name: "Pity Song", path: "${manager.renamedFilesDirectory}/Pity Song.opus", status: FileStatus.Renamed);
+    var newName = "Pity Song";
+    var renamedAudicker = await manager.rename(audicker, newName);
+    
+
+    expect(renamedAudicker, newAudicker);
+
+  });
+}
