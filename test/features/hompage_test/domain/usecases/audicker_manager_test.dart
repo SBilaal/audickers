0b94fd2cacf67a3d599aae872f8774e6ede8b64b
SBilaal <mbilaalme@gmail.com>
2020-09-03 18:42:54 +0100

    Adjusted tests to reflect audicker model change.

--- test/features/hompage_test/domain/usecases/audicker_manager_test.dart
+++ test/features/hompage_test/domain/usecases/audicker_manager_test.dart
@@ -15,19 +15,19 @@ void main() {
   setUp((){
     repository = MockAudickerRepository();
     manager = AudickerManager(repository);
-    audicker = Audicker(name: "aud", path: "Whatsapp/aud.opus", status: FileStatus.Raw);
+    audicker = Audicker(name: "aud", path: "Whatsapp/aud.opus", status: FileStatus.Raw, duration: Future.value(Duration.zero));
   });
 
   test("should rename the file passed into it", () async {
-    var newAudicker = Audicker(name: "Pity Song", path: "${manager.renamedFilesDirectory}/Pity Song.opus", status: FileStatus.Renamed);
+    var newAudicker = Audicker(name: "Pity Song", path: "${manager.renamedFilesDirectory}/Pity Song.opus", status: FileStatus.Renamed, duration: Future.value(Duration.zero));
     var newName = "Pity Song";
     var renamedAudicker = await manager.rename(audicker, newName);
     expect(renamedAudicker, newAudicker);
   });
 
   test("should get a list of raw audios", () async {
-    var audicker1 = Audicker(name: "Audicker1", path: "Whatsapp/Audicker1.opus", status: FileStatus.Raw);
-    var audicker2 = Audicker(name: "Audicker2", path: "Whatsapp/Audicker2.opus", status: FileStatus.Raw);
+    var audicker1 = Audicker(name: "Audicker1", path: "Whatsapp/Audicker1.opus", status: FileStatus.Raw, duration: Future.value(Duration.zero));
+    var audicker2 = Audicker(name: "Audicker2", path: "Whatsapp/Audicker2.opus", status: FileStatus.Raw, duration: Future.value(Duration.zero));
     var listOfAudickers = [audicker1, audicker2]; 
 
     when(repository.getRawAudickers()).thenAnswer((_) async => listOfAudickers);
@@ -37,8 +37,8 @@ void main() {
   });
 
   test("should get a list of renamed audios", () async {
-    var audicker1 = Audicker(name: "Pity Song", path: "${manager.renamedFilesDirectory}/Pity Song.opus", status: FileStatus.Renamed);
-    var audicker2 = Audicker(name: "Lmao", path: "${manager.renamedFilesDirectory}/Lmao.opus", status: FileStatus.Renamed);
+    var audicker1 = Audicker(name: "Pity Song", path: "${manager.renamedFilesDirectory}/Pity Song.opus", status: FileStatus.Renamed, duration: Future.value(Duration.zero));
+    var audicker2 = Audicker(name: "Lmao", path: "${manager.renamedFilesDirectory}/Lmao.opus", status: FileStatus.Renamed, duration: Future.value(Duration.zero));
     var listOfAudickers = [audicker1, audicker2]; 
 
     when(repository.getRenamedAudickers()).thenAnswer((_) async => listOfAudickers);
