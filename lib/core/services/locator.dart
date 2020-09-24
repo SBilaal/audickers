7869ea504662cc6bafc63ee4a88bb243d9b9076c
SBilaal <mbilaalme@gmail.com>
2020-09-24 10:51:22 +0100

    created a locator.dart class for service location.

---
+++ lib/core/services/locator.dart
@@ -0,0 +1,13 @@
+import 'package:audickers/core/services/audio_player.dart';
+import 'package:audickers/core/services/search.dart';
+import 'package:audickers/features/homepage/data/repositories/audicker_repository_impl.dart';
+import 'package:audickers/features/homepage/domain/usecases/audicker_manager.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt.I;
+
+setup() {
+  locator.registerLazySingleton(() => Search());
+  locator.registerLazySingleton(() => AudickerManager(AudickerRepositoryImpl()));
+  locator.registerLazySingleton(() => AudioPlayer());
+}
