bed01dc2f747b7eb6f357898e22117d92d972f0c
SBilaal <mbilaalme@gmail.com>
2020-09-28 11:13:08 +0100

    Added AppTheme to the service locator.

--- lib/core/services/locator.dart
+++ lib/core/services/locator.dart
@@ -2,6 +2,7 @@ import 'package:audickers/core/services/audio_player.dart';
 import 'package:audickers/core/services/search.dart';
 import 'package:audickers/features/homepage/data/repositories/audicker_repository_impl.dart';
 import 'package:audickers/features/homepage/domain/usecases/audicker_manager.dart';
+import 'package:audickers/features/homepage/presentation/app_theme.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.I;
@@ -10,4 +11,5 @@ setup() {
   locator.registerLazySingleton(() => Search());
   locator.registerLazySingleton(() => AudickerManager(AudickerRepositoryImpl()));
   locator.registerLazySingleton(() => AudioPlayer());
+  locator.registerLazySingleton(() => AppTheme());
 }
